f67e4092647ede7cdc995ae149f3f847a06e4186
Lars Wikberg <lars.wikberg@gmail.com>
2014-03-04 22:08:26 +0200

    default mouse pointer xmodmap

--- .mouse.sh
+++ .mouse.sh
@@ -3,6 +3,7 @@
 # MOUSE SETTINGS
 # pointer = 1/left 2/middle 3/right 4/scrollup 5/scrolldown X X 8/back 9/forward 10/högstoppmitten?!
 xset m 1
+#xmodmap -e "pointer = 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16" # default
 xmodmap -e "pointer = 1 9 3 4 5 6 7 2 8" # logitech g400 # back -> middlem, forward -> back
 # xmodmap -e "pointer = 1 9 3 4 5 6 7 2 8 10" # logitech g5 1st edition
 xinput --set-prop "Logitech Gaming Mouse G400" "Device Accel Profile" -1 # remove acceleration
