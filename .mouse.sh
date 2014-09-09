a106081e4f73433f9c0a1928a055eb5ac7ed1ffc
Lars Wikberg <lars.wikberg@gmail.com>
2014-09-09 17:50:33 +0300

    mouse updates

--- .mouse.sh
+++ .mouse.sh
@@ -2,10 +2,17 @@
 
 # MOUSE SETTINGS
 # pointer = 1/left 2/middle 3/right 4/scrollup 5/scrolldown X X 8/back 9/forward 10/högstoppmitten?!
-xset m 1
+
+#xset m 3
 #xmodmap -e "pointer = 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16" # default
+
+xset m 1
 xmodmap -e "pointer = 1 9 3 4 5 6 7 2 8" # logitech g400 # back -> middlem, forward -> back
 # xmodmap -e "pointer = 1 9 3 4 5 6 7 2 8 10" # logitech g5 1st edition
-xinput --set-prop "Logitech Gaming Mouse G400" "Device Accel Profile" -1 # remove acceleration
-xinput --set-prop "Logitech USB Gaming Mouse" "Device Accel Profile" -1 # remove acceleration
+#xinput --set-prop "Logitech Gaming Mouse G400" "Device Accel Profile" -1 # remove acceleration
+#xinput --set-prop "Logitech USB Gaming Mouse" "Device Accel Profile" -1 # remove acceleration G5 first gen
+xinput --set-prop "Logitech G400s Optical Gaming Mouse" "Device Accel Profile" -1 # remove acceleration
+
+# logitech T650
+xinput set-ptr-feedback "Logitech Unifying Device. Wireless PID:4101" 10 10 4
 
